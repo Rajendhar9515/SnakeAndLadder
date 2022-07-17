f130a894d4a23bbb2047e9a9fc3e9ee3716123cf
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-17 22:40:01 +0530

    added Program

--- StartPosition.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.java.learningproblem;
-
-public class StartPosition {
-	public static void main(String args[]) {
-		int StartPosition = 0;
-		System.out.println("Start the Snake and Ladder Game");
-	}
-
-}
